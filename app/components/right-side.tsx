3dc32e1071dd281bca2b9f08f92d915a9047e78d
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-15 18:11:50 +0100

    update: home page and right-side component

---
+++ app/components/right-side.tsx
@@ -0,0 +1,3 @@
+export const RightSide = ({ children }: { children: React.ReactNode }) => (
+  <section className='pt-24 lg:w-1/2 lg:py-24'>{children}</section>
+);
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { GridContainer } from './components/grid-container';
 import { Header } from './components/header';
+import { RightSide } from './components/right-side';
 
 export default function Home() {
   return (
     <GridContainer>
       <Header />
+      <RightSide>Hello from right side</RightSide>
     </GridContainer>
   );
 }
